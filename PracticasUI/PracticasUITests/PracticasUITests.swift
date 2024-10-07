3ee78a9c21e870f211399ad27e70d43625f8c8e8
yagoaresarmesto <59642410+yagoaresarmesto@users.noreply.github.com>
2024-10-07 15:54:54 +0200

    Initial config

---
+++ PracticasUI/PracticasUITests/PracticasUITests.swift
@@ -0,0 +1,17 @@
+//
+//  PracticasUITests.swift
+//  PracticasUITests
+//
+//  Created by Ares Armesto, Yago on 7/10/24.
+//
+
+import Testing
+@testable import PracticasUI
+
+struct PracticasUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ PracticasUI/PracticasUIUITests/PracticasUIUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  PracticasUIUITestsLaunchTests.swift
+//  PracticasUIUITests
+//
+//  Created by Ares Armesto, Yago on 7/10/24.
+//
+
+import XCTest
+
+final class PracticasUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
