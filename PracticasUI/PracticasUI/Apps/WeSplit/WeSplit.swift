b7935509428a49bd4efad04fc04bbf78b765ac6a
yagoaresarmesto <59642410+yagoaresarmesto@users.noreply.github.com>
2024-10-09 17:04:13 +0200

    Updating basics

---
+++ PracticasUI/PracticasUI/Apps/WeSplit/WeSplit.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+struct WeSplit: View {
+    let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = "Harry"
+
+     var body: some View {
+         NavigationStack {
+             Form {
+                 Picker("Select your student", selection: $selectedStudent) {
+                     ForEach(students, id: \.self) {
+                         Text($0)
+                     }
+                 }
+             }
+         }
+     }
+ }
+
+#Preview {
+    WeSplit()
+}
--- PracticasUI/PracticasUI/PracticasUIApp.swift
+++ PracticasUI/PracticasUI/PracticasUIApp.swift
@@ -1,17 +1,10 @@
-//
-//  PracticasUIApp.swift
-//  PracticasUI
-//
-//  Created by Ares Armesto, Yago on 7/10/24.
-//
-
 import SwiftUI
 
 @main
 struct PracticasUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            WeSplit()
         }
     }
 }
