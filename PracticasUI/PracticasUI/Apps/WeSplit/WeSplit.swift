eb2d0bc462106236333ea26cb7a8a2519ce51683
yagoaresarmesto <59642410+yagoaresarmesto@users.noreply.github.com>
2024-10-10 17:41:58 +0200

    Pequeña práctica para ver componentes

--- PracticasUI/PracticasUI/Apps/WeSplit/WeSplit.swift
+++ PracticasUI/PracticasUI/Apps/WeSplit/WeSplit.swift
@@ -1,20 +1,57 @@
 import SwiftUI
 
 struct WeSplit: View {
-    let students = ["Harry", "Hermione", "Ron"]
-    @State private var selectedStudent = "Harry"
 
-     var body: some View {
-         NavigationStack {
-             Form {
-                 Picker("Select your student", selection: $selectedStudent) {
-                     ForEach(students, id: \.self) {
-                         Text($0)
-                     }
-                 }
-             }
-         }
-     }
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+
+    var body: some View {
+        NavigationStack{
+            Form{
+                Section{
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $numberOfPeople){
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                    
+                }
+                
+                Section("How much do you want to tip?") {
+                    Picker("Tip percentage", selection: $tipPercentage){
+                        ForEach(tipPercentages, id: \.self){
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                
+            }
+            .navigationTitle("WeSplit")
+        }
+    }
  }
 
 #Preview {
