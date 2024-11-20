d7a06ea5dc62675c4b5988793a3c0148302b4614
yagoaresarmesto <59642410+yagoaresarmesto@users.noreply.github.com>
2024-11-20 19:56:01 +0100

    WordScarmble terminado

--- PracticasUI/PracticasUI/Apps/7. Word Scramble/WordScramble.swift
+++ PracticasUI/PracticasUI/Apps/7. Word Scramble/WordScramble.swift
@@ -5,6 +5,10 @@ struct WordScramble: View {
     @State private var rootWord = ""
     @State private var newWord = ""
     
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
     var body: some View {
         NavigationStack {
             List {
@@ -24,6 +28,12 @@ struct WordScramble: View {
             }
             .navigationTitle(rootWord)
             .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
+            .alert(errorTitle, isPresented: $showingError){
+                Button("OK"){} //Si borro esto tmb crea por defecto un ok button
+            } message: {
+                Text(errorMessage)
+            }
         }
     }
     
@@ -34,13 +44,71 @@ struct WordScramble: View {
         
         guard answer.count > 0 else { return }
         
-        //Extra validation to come
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
+        
+        guard isPosible(word: answer) else {
+            wordError(title: "Word not posible", message: "You cant spell that word from '\(rootWord)'!")
+            return
+        }
+        
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
         
         withAnimation{
             usedWords.insert(answer, at: 0) //añadimos la respuesta al array de strings
         }
-
+        
         newWord = "" // Seteamos la palabra otr vez vacía
+    }
+    
+    //Función para que coga del .txt una palabra aleatoria
+    func startGame(){
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt"){
+            if let startWords = try? String(contentsOf: startWordsURL){
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkworm"
+                return
+            }
+        }
+        
+        fatalError("Could not load start.txt from bundle.")
+        
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPosible(word:String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter ){
+                tempWord.remove(at: pos)
+            }else{
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String){
+        errorTitle = title
+        errorMessage = message
+        showingError = true
     }
 }
 
